b4189e2226e5c46fc99e46d7c1a0893217ddf159
Plombirka <61332527+Plombirka@users.noreply.github.com>
2023-01-25 12:20:23 +0300

    Add files via upload

---
+++ DKR_7.pas
@@ -0,0 +1,38 @@
+﻿uses GraphABC;
+uses fractals;
+var y,x,n,g:integer;
+ procedure KeyDown(key: integer);
+begin
+case key of
+VK_Up: begin y := y - 5; end;
+VK_Down: begin y += 5; end;
+VK_Left: begin x := x - 5; end;
+VK_Right: begin x := x + 5; end;
+VK_A: x := x - 50;
+VK_D: x := x + 50;
+VK_W: y := y - 50;
+VK_S: y := y + 50;
+VK_H: n -= 5;
+VK_J: n += 5;
+VK_U: g += 1;
+VK_Y: g -= 1;
+end;
+Window.Clear;
+serp_square(x, y, n, g);
+redraw;
+end;
+begin
+    Window.Maximize;
+    var center := window.center;
+    moveto(center.x - 200, center.y - 100);
+    a := 0;
+    n:=300;
+    g:=2;
+    writeln('Смена глубина на "Y" и "U"(Y-уменьшение, U-увеличение)');
+    writeln('Смена масштаба на "H" и "J"(H-уменьшение J-увеличение)');
+    writeln('Перемешение на стрелочки и WASD');
+    sleep(5000);
+    window.Clear;
+    serp_square(x, y, n, g);
+    onKeyDown += keydown;
+end.
---
+++ fractals.pas
@@ -0,0 +1,26 @@
+﻿unit fractals;
+interface
+uses GraphABC;
+var
+    a: real;
+procedure serp_square(x, y, l, n: integer; first: boolean := True);
+implementation
+procedure serp_square;
+var
+    third_part := (l div 3);
+begin
+    if first then
+    begin
+        SetBrushColor(clBlack);
+        FillRectangle(x, y, x + l, y + l);
+        SetBrushColor(clWhite);
+        first := False;
+    end;
+    FillRectangle(x + third_part, y + third_part, x + 2 * third_part, y + 2 * third_part);
+    if (n <= 0) then exit;
+    for i: integer := 0 to 2 do
+        for j: integer := 0 to 2 do
+            if (i <> j) or (i <> 1) then
+                serp_square(x + third_part * i, y + third_part * j, third_part, n - 1);
+end;
+end.
